8592530527a2828b60a5d45863a2beb0e89b051b
betipp <betipisc@gmail.com>
2023-01-06 11:03:21 +0100

    added desk bell that completes tasks
    Desk bell completes tasks and resets them

--- Assets/Scripts/ListManager.cs
+++ Assets/Scripts/ListManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class ListManager : MonoBehaviour
 {
 
-    private string[] List = new string[3];
+    public string[] List = new string[3];
     [SerializeField]
     public string[] AllTasks = { };
     public bool[] ListStatus = { false, false, false };
@@ -16,10 +16,13 @@ public class ListManager : MonoBehaviour
     [SerializeField]
     public ParticleSystem stars;
 
+    private GameObject[] orderItems = { null, null, null };
+
 
 
     void OnTriggerEnter(Collider other)
     {
+
         for (int i = 0; i < List.Length; i++)
         {
             //Go thru the list of taska and figure out if the colliding object in one of them, if that is so consider the task done
@@ -29,17 +32,23 @@ public class ListManager : MonoBehaviour
                 AudioManager.Play("Done");
                 stars.Play();
                 printTasks();
+                orderItems[i] = other.gameObject;
                 other.tag = "TaskDone";
+
             }
         }
     }
+
+
+
+
 
     void Start()
     {
         fillListWithTasks();
     }
 
-    void fillListWithTasks()
+    public void fillListWithTasks()
     {
         int tasks = 0;
         while (tasks < List.Length)
@@ -88,9 +97,26 @@ public class ListManager : MonoBehaviour
                 return true;
             }
         }
-
-
         return false;
-
+    }
+    public void removeOrderItems()
+    {
+        for (int i = 0; i < orderItems.Length; i++)
+        {
+            Destroy(orderItems[i]);
+        }
+        orderItems = new GameObject[] { null, null, null };
+        ListStatus = new bool[] { false, false, false };
+    }
+    public bool areAllTrue()
+    {
+        for (int i = 0; i < ListStatus.Length; ++i)
+        {
+            if (ListStatus[i] == false)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
