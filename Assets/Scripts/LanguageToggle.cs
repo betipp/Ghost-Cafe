571e8f4e5feeb524acf9c6e2fe8be1a12e551ef3
betipp <betipisc@gmail.com>
2024-01-11 17:21:46 +0100

    Added SLO support on main menu
    -when switching languages the main menu language also changes

--- Assets/Scripts/LanguageToggle.cs
+++ Assets/Scripts/LanguageToggle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +13,18 @@ public class LanguageToggle : MonoBehaviour
     public UnityEngine.UI.Image SLOImage;
     [SerializeField]
     public UnityEngine.UI.Image ENGImage;
+
+    //Main menu
+    [SerializeField]
+    public TMPro.TextMeshProUGUI startText;
+    [SerializeField]
+    public TMPro.TextMeshProUGUI settingsText;
+    [SerializeField]
+    public TMPro.TextMeshProUGUI exitText;
+    [SerializeField]
+    public TMPro.TextMeshProUGUI backText;
+
+
 
 
 
@@ -39,15 +52,36 @@ public class LanguageToggle : MonoBehaviour
 
     private void toggleImages()
     {
+        //SLO
         if (translation)
         {
             SLOImage.enabled = true;
             ENGImage.enabled = false;
-        }
+        }//ENG
         else
         {
             SLOImage.enabled = false;
             ENGImage.enabled = true;
+        }
+        toggleMainMenuText();
+    }
+
+    private void toggleMainMenuText()
+    {
+        //SLO
+        if (translation)
+        {
+            startText.text = "Start";
+            settingsText.text = "Nastavitve";
+            exitText.text = "Izhod";
+            backText.text = "Nazaj";
+        }//ENG
+        else
+        {
+            startText.text = "Start";
+            settingsText.text = "Settings";
+            exitText.text = "Exit";
+            backText.text = "Back";
         }
     }
 
